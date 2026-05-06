98cd54dc476c7d2186893f84be938ece9665215b
athulyagireesh <athulyatg721@gmail.com>
2026-05-06 12:21:19 +0530

    day 20

--- Zecpath_Project/core/serializers.py
+++ Zecpath_Project/core/serializers.py
@@ -57,11 +57,7 @@ class EmployerSerializer(serializers.ModelSerializer):
 
 
 
-# class ApplicationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Application
-#         fields = '__all__'
-#         read_only_fields = ['candidate', 'status']
+
 
 
 class ApplicationSerializer(serializers.ModelSerializer):
